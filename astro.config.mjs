86a8df32872d525596eb9dad50f3b964f46c6081
lichaochao <lichaochao@chanwind.com>
2024-09-04 14:21:56 +0800

    模版部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,10 @@ export default defineConfig({
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
+{
+					label: 'Sleep',
+					autogenerate: { directory: 'sleep' },
+				},
 			],
 		}),
 	],
